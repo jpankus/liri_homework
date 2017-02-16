4a25514dc17b8b4f2a7f027ba43e4b8eeb125a57
Jessica Pankus <jessica@Jessicas-MacBook-Pro.local>
2017-02-15 20:08:15 -0500

    load files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'dpWTGD4RaUtKw5CEWnXqb4sJm',
+  consumer_secret: 'iLxuTozG6JeKW5033TTkxIu9EYDvJsxj7BUu29sxtkA1oOtixZ',
+  access_token_key: '402870602-g3k6QkKrmpSa2yK9Bc7qff4OGyRSsLNsNowSSqoO',
+  access_token_secret: 'OaVAaIUZXyPcxKAgNLBXFtMA7b88TxFWD2BPAregSHhrj',
+}
